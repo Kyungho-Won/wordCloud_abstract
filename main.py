07acb6630a37aa602f772536a95af10be4cd51d6
Kyung-Ho Won <kywon@ad.inria.fr>
2024-01-09 10:12:46 +0100

    Add noun extraction and filter the unwanted words

--- main.py
+++ main.py
@@ -2,10 +2,19 @@
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+import nltk # extract noun from string
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+
+#%%
+
+def filter_strings(string_list, words_to_remove):
+    filtered_list = [s for s in string_list if not any(word in s for word in words_to_remove)]
+    return filtered_list
 
 #%% Example: List of abstracts (replace with your actual data)
 df = pd.read_csv('./data/scopus_all.csv')
-print(df) 
 
 """
 abstracts = [
@@ -16,6 +25,9 @@ abstracts = [
 ]
 """
 
+list_exclude = ['EEG', 'brain', 'BCI', 'brain-computer interface', 'interface', 'neurofeedbac', 'result', 'method',
+                'study', 'system']
+
 # Available fields: 'Abstract' or 'Title'
 abstracts = df['Title']
 # Combine the abstracts into a single string
@@ -25,7 +37,24 @@ all_abstracts = " ".join(abstracts)
 wordcloud = WordCloud(width=800, height=400, background_color="white").generate(all_abstracts)
 
 # Display the generated word cloud using matplotlib
+"""
 plt.figure(figsize=(10, 5))
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis("off")
 plt.show()
+"""
+
+# Extract noun
+nouns_extracted = [word for (word, pos) in nltk.pos_tag(nltk.word_tokenize(all_abstracts)) if pos[0] == 'N']
+nouns_filtered = filter_strings(nouns_extracted, list_exclude)
+all_nouns = " ".join(nouns_filtered)
+
+# Create a WordCloud object
+wordcloud = WordCloud(width=800, height=400, background_color="white").generate(all_nouns)
+
+# Display the generated word cloud using matplotlib
+plt.figure(figsize=(10, 5))
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()
+
